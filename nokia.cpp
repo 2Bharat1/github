13e272eb9405d9e67659a9859c22606014d374d0
2Bharat1 <109964995+2Bharat1@users.noreply.github.com>
2022-07-25 18:39:56 +0545

    Add files via upload

---
+++ nokia.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a ,i;
+	cout<<"enter the numbers"<<endl;
+	
+	cin>>a;
+	
+	
+	switch(a){
+		case 1:
+			cout<<"A"<<endl;
+			break;
+			
+			case 11:
+				cout<<"B"<<endl;
+				break;
+				
+				
+				case 111:
+				cout<<"C"<<endl;
+				break;
+					case 1111:
+			cout<<"D"<<endl;
+			break;
+			 
+			 	case 2:
+			cout<<"E"<<endl;
+			break;
+				case 22:
+			cout<<"F"<<endl;
+			break;
+				case 222:
+			cout<<"G"<<endl;
+			break;
+				case 2222:
+			cout<<"H"<<endl;
+			break;
+				case 3:
+			cout<<"I"<<endl;
+			break;
+				case 33:
+			cout<<"J"<<endl;
+			break;
+				case 333:
+			cout<<"K"<<endl;
+			break;
+				case 3333:
+			cout<<"L"<<endl;
+			break;
+			
+					case 4:
+			cout<<"M"<<endl;
+			break;
+				case 44:
+			cout<<"N"<<endl;
+			break;
+				case 444:
+			cout<<"O"<<endl;
+			break;
+				case 5:
+			cout<<"P"<<endl;
+			break;
+				case 55:
+			cout<<"Q"<<endl;
+			break;
+				case 555:
+			cout<<"R"<<endl;
+			break;
+				case 6:
+			cout<<"S"<<endl;
+			break;
+				case 66:
+			cout<<"T"<<endl;
+			break;
+				case 666:
+			cout<<"U"<<endl;
+			break;
+				case 7:
+			cout<<"U"<<endl;
+			break;
+				case 77:
+			cout<<"V"<<endl;
+			break;
+				case 777:
+			cout<<"W"<<endl;
+			break;
+				case 8:
+			cout<<"X"<<endl;
+			break;
+				case 88:
+			cout<<"Y"<<endl;
+			break;
+				case 888:
+			cout<<"Y"<<endl;
+			break;
+				case 9:
+			cout<<"Z"<<endl;
+			break;
+			
+				default :
+					cout<<"end";
+					break;
+			}
+				return 0;
+			}
+			
+
+
+
